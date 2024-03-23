4cf470a7a5f53cdb813ce64e338da2b1e60fc76f
Blenderis85 <agedvilasplay@gmail.com>
2024-03-23 18:56:39 +0200

    Update

--- Python_Basics/Test_Debug/main3.py
+++ Python_Basics/Test_Debug/main3.py
@@ -1,5 +1,4 @@
 def unlock_achievement(before_xp, ach_xp, ach_name):
-    # Calculate player's xp after the achievement is unlocked
     after_xp = before_xp + ach_xp
     
     # Generate the alert message
